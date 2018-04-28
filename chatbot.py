ca78caf922eb5fa0779d2f3e878c0a800f2b48aa
Jose Manuel Cordoba <33335341+JoseCordobaEAN@users.noreply.github.com>
2018-04-28 12:05:44 -0500

    Archivos base para la realización del taller

---
+++ chatbot.py
@@ -0,0 +1,11 @@
+# TODO Crear Variable con preguntas por defecto
+
+# TODO Preguntar al usuario su pregunta
+
+# TODO Si se despide debemos cerrar
+
+# TODO Si conocemos la respuesta debemos responder
+
+# TODO Si no conocemos la respuesta debemos aprender
+
+# TODO Repetir hasta que el usuario se despida
